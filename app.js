984436ec8c5f5201e7ddefdd53e7f80e14fe3f50
Denny Rubino <rubidenny@gmail.com>
2025-03-12 01:04:34 +0100

    feat:controllers, routes and call api

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Server del sito");
+app.use(express.json());
+
+const logger = require("./middleware/logger");
+app.use(logger);
+
+const productRoutes = require("./routes/productRoutes");
+app.use("/api/products", productRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Errore interno al server" });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server in ascolto sulla porta ${PORT}`);
 });
