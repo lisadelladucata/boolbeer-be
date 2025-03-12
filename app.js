a2c27ba1f3f6ba64473e20ec3c1ce0cf57ab576a
Denny Rubino <rubidenny@gmail.com>
2025-03-13 00:24:09 +0100

    add: fe and be connection

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const express = require("express");
 const notFound = require("./middleware/notFound");
 const handleErrors = require("./middleware/handleErrors");
 const logger = require("./middleware/logger");
-const app = express();
 const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const FE_CLIENT = process.env.FE_CLIENT;
 
-app.use(cors({ origin: process.env.FE_CLIENT }));
+app.use(
+  cors({
+    origin: FE_CLIENT,
+  })
+);
+
 app.use(express.json());
 app.use(logger);
 
