124f870c12af48add78f421c00498bfcffd9f112
Umberto Amenta <umberto.amenta@hotmail.it>
2025-03-19 22:15:52 +0100

    better slug

--- utils/slug.js
+++ utils/slug.js
@@ -1,5 +1,8 @@
 const generateSlug = (name) => {
   return name
+    .replace(/'/g, "")
+    .normalize("NFD") // elimina gli accenti
+    .replace(/[\u0300-\u036f]/g, "") // elimina segni diacritici
     .toLowerCase()
     .replace(/[^a-z0-9]+/g, "-")
     .replace(/(^-|-$)/g, "");
